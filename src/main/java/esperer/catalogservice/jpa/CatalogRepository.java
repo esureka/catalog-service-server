7541ca3be93c8b909c97525b6835636430a1a235
aesperer <piyrw9754@gmail.com>
2023-07-25 20:27:06 +0900

    catalog dto, response

---
+++ src/main/java/esperer/catalogservice/jpa/CatalogRepository.java
@@ -0,0 +1,9 @@
+package esperer.catalogservice.jpa;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface CatalogRepository extends CrudRepository<CatalogEntity, Long> {
+    Optional<CatalogEntity> findByProductId(String productId);
+}
